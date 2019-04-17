81041cb22e371c6f32b1fef1d0b6bfe960bc9efb
jseutens <johan@seutens.eu>
2019-04-18 00:31:56 +0200

    Update iptables-abuseipdb.sh

--- iptables-abuseipdb.sh
+++ iptables-abuseipdb.sh
@@ -3,15 +3,26 @@ date
 # https://www.howtoforge.com/tutorial/protect-your-server-computer-with-badips-and-fail2ban/
 # based on this version http://www.timokorthals.de/?p=334
 _ipt=/sbin/iptables    # Location of iptables (might be correct)
-_input=badips.db       # Name of database (will be downloaded with this name)
-_pub_if=eth0           # Device which is connected to the internet (ex. $ifconfig for that)
-_droplist=droplist     # Name of chain in iptables (Only change this if you have already a chain with this name)
+_iptv6=/sbin/ip6tables    # Location of iptables (might be correct)
 # get your api key from https://www.abuseipdb.com/account/plans (the free plan lets you download the blacklist 10 times a day)
 YOUR_API_KEY="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 # check if chain exists
 CHAIN_NAME="blacklist-abuseipdb"
+CHAIN_NAMEv6="blacklist-abuseipdbv6"
 CHAIN_NAME_FILE="/tmp/abuseipdb.txt";
 # if iptables version >=1.4.x, you may use -S flag, not -L
+#v6
+if $_iptv6 -S $CHAIN_NAMEv6 > /dev/null
+then
+  #echo chain $CHAIN_NAME exists
+  # flush the chain to repopulate with new downloaded ip list
+  $_iptv6 -F $CHAIN_NAMEv6
+else
+  # echo chain $CHAIN_NAME not exists
+  # create the chain with iptables
+  $_iptv6 -N $CHAIN_NAME
+fi
+#v4
 if $_ipt -S $CHAIN_NAME > /dev/null
 then
   #echo chain $CHAIN_NAME exists
@@ -41,6 +52,19 @@ COMBINEDIPV6="/tmp/abuseipdbipv6.txt"
         echo $IP >> $COMBINEDIPV4
       fi
 done
+# first v6 , v4 will be added later before v6 , so v4 has better possibility to match so no v6 rules will be used as its match a blocking , less memory use
+#insert ipv6 IP
+for IPv6 in $( cat $COMBINEDIPV6 ); do
+  $_iptv6 -A $CHAIN_NAMEv6 -s $IPv6 -j LOG --log-prefix "ABUSEDBIP"
+  $_iptv6 -A $CHAIN_NAMEv6 -s $IPv6 -j DROP
+printf '.'
+done
+#
+# Finally, insert or append our black list v6
+$_iptv6 -I INPUT -j $CHAIN_NAMEv6
+$_iptv6 -I OUTPUT -j $CHAIN_NAMEv6
+$_iptv6 -I FORWARD -j $CHAIN_NAMEv6
+#
 #
 #insert ipv4 IP
 for IP in $( cat $COMBINEDIPV4 ); do
